a651610cf159715bb3a5f970d0853f72ce3f089e
João Victor <jvictorgmodesto@gmail.com>
2017-09-28 17:23:39 -0300

    Token recolhe ip e usuario que logou

--- bootstrap.php
+++ bootstrap.php
@@ -96,9 +96,12 @@ $app->add(new \Slim\Middleware\JwtAuthentication([
             ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
     }
 
-
-
 ]));
+
+//Middleware para pegar o ip do usuario
+$checkProxyHeaders = true;
+$trustedProxies = ['10.0.0.1', '10.0.0.2'];
+$app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
 
 
  ?>
--- index.php
+++ index.php
@@ -12,31 +12,31 @@ require 'Routes/LoginRoutes.php';
 require 'Routes/DenunciaRoutes.php';
 require 'Routes/ComentarioRoutes.php';
 
+
+
 /**
  * HTTP Auth - Autenticação minimalista para retornar um JWT
  */
-
-
 $app->get('/auth', function (Request $request, Response $response) use ($app) {
-    $key = $this->get("secretkey");
 
+    $key = $this->get("secretkey");
 
     $now = new DateTime();
     $future = new DateTime("now +1 hour");
-
+    $ipAddress = $request->getAttribute('ip_address');
+    $user = $_SERVER['PHP_AUTH_USER'];
+    $password = $_SERVER['PHP_AUTH_PW'];
 
 
     $token = array(
-        "user" => "Root",
-        "senha" => "CareCity",
+        "user" => $user,
+        "senha" => $password,
         "exp" => $future->getTimeStamp(),
-        "ip" => 
+        "ip" => $ipAddress
 
     );
 
-
     $jwt = JWT::encode($token,$key);
-
 
     return $response->withJson(["auth-jwt" => $jwt], 200)
         ->withHeader('Content-type', 'application/json');
