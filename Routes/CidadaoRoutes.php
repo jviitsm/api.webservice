617371bf6432b7589f5a7aeff1b1989dc83e18d5
João Victor Modesto <jvictorgmodesto@gmail.com>
2017-10-14 22:47:21 -0300

    Testar cadastro de imagem junto com cadastro de cidadao

--- Routes/CidadaoRoutes.php
+++ Routes/CidadaoRoutes.php
@@ -3,151 +3,130 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use App\Models\Entity\Login;
 use App\Models\Entity\Cidadao;
+$app ->post('/cidadao/cadastrar', function(Request $request, Response $response) use ($app){
+    //Container do EntityManager
+    $entityManager = $this -> get('em');
 
-$app -> post('/cidadao/cadastrar', function(Request $request, Response $response) use ($app){
-
-    if(!$request->getParsedBody()){
-        throw new Exception("Corpo de requisição vazio", 204);
-    } else {
-        //Container do EntityManager
-        $entityManager = $this -> get('em');
-        try{
-            //Instância da entidade Login
-            $login = new Login();
-            //recuperando o parâmetro objeto login do json
-            $fk_login_cidadao = $request->getParam('fk_login_cidadao');
-            //setando valores do objeto login
-            $login ->setLogin($fk_login_cidadao['login']);
-            $login ->setEmail($fk_login_cidadao['email']);
-            $login ->setSenha($fk_login_cidadao['senha']);
-            $login ->setStatus_login($fk_login_cidadao['status_login']);
-            $login ->setAsAdministrador($fk_login_cidadao['administrador']);
-            //salvando login
-            $entityManager->persist($login);
-            $entityManager->flush();
-            //buscando login recém salvo
-            $loginRepository = $entityManager->getRepository('App\Models\Entity\Login');
-            //pegando login
-            $loginCidadao = $loginRepository->find($login->getId_login());
+    $json = $request->getParam('cidadao');
+    $json = json_decode($json);
 
 
-            //Salvar foto
-            $files = $request->getUploadedFiles();
-            $newimage = $files['foto'];
+    try{
+        //Instância da entidade Login
+        $login = new Login();
+        //recuperando o parâmetro objeto login do json
+        $fk_login_cidadao = $json -> fk_login_cidadao;
+        //setando valores do objeto login
+        $login ->setLogin($fk_login_cidadao -> login);
+        $login ->setEmail($fk_login_cidadao -> email);
+        $login ->setSenha($fk_login_cidadao -> senha);
+        $login ->setStatus_login($fk_login_cidadao -> status_login);
+        $login ->setAsAdministrador($fk_login_cidadao -> administrador);
 
-            if ($newimage->getError() === UPLOAD_ERR_OK) {
-                $uploadFileName = $newimage->getClientFilename();
-                $type = $newimage->getClientMediaType();
-                $name = uniqid('img-' . date('d-m-y') . '-');
-                $name .= $newimage->getClientFilename();
-                //  $imgs[] = array('url' => '/Photos/' . $name);
-
-                //local server
-
-                $newimage->moveTo("/home/citycare/public_html/Imgs/user/$name");#/home/citycare/Imgs/User/$name
-
-                //localdev
-                $photoURL = "/home/citycare/public_html/Imgs/user/$name";#/home/citycare/Imgs/User/$name
-
-            }
-
-            //Instância da entidade Cidadao
-            $cidadao = new Cidadao();
-            //setando valores do objeto cidadao
-            $cidadao ->setFk_login_cidadao($loginCidadao);
-            $cidadao->setNome($request->getParam('nome'));
-            $cidadao->setSexo($request->getParam('sexo'));
-            $cidadao ->setSobrenome($request->getParam('sobrenome'));
-            $cidadao ->setEstado($request->getParam('estado'));
-            $cidadao ->setCidade($request->getParam('cidade'));
-            $cidadao ->setDir_foto_usuario($request->getParam($photoURL));
-            $entityManager->persist($cidadao);
-            $entityManager->flush();
-            //retornando confirmação do evento completo
-            $return = $response->withJson(["result" => true],201)->withHeader('Content-type', 'application/json');
-            
-            
-        } catch (Exception $ex){
-            //código e mensagem do erro
-            throw new Exception($ex->getMessage(), $ex->getCode());
+        //salvando login
+        $entityManager->persist($login);
+        $entityManager->flush();
+        //buscando login recém salvo
+        $loginRepository = $entityManager->getRepository('App\Models\Entity\Login');
+        //pegando login
+        $loginCidadao = $loginRepository->find($login->getId_login());
+        //Salvar foto
+        $files = $request->getUploadedFiles();
+        $newimage = $files['foto'];
+        if ($newimage->getError() === UPLOAD_ERR_OK) {
+            $uploadFileName = $newimage->getClientFilename();
+            $type = $newimage->getClientMediaType();
+            $name = uniqid('img-' . date('d-m-y') . '-');
+            $name .= $newimage->getClientFilename();
+            //  $imgs[] = array('url' => '/Photos/' . $name);
+            //local server
+            $newimage->moveTo("/home/citycare//imgs/$name");#/home/citycare/Imgs/User/$name
+            //localdev
+            $photoURL = "/home/citycare//imgs/$name";#/home/citycare/Imgs/User/$name
         }
-        return $return;
+        //Instância da entidade Cidadao
+        $cidadao = new Cidadao();
+        //setando valores do objeto cidadao
+        $cidadao ->setFk_login_cidadao($loginCidadao);
+        $cidadao->setNome($json -> nome);
+        $cidadao->setSexo($json -> sexo);
+        $cidadao ->setSobrenome($json -> sobrenome);
+        $cidadao ->setEstado($json -> estado);
+        $cidadao ->setCidade($json -> cidade);
+        $cidadao ->setDir_foto_usuario($photoURL);
+        $entityManager->persist($cidadao);
+        $entityManager->flush();
+        //retornando confirmação do evento completo
+        $return = $response->withStatus(201)->withHeader('Content-type', 'application/json');
+
+
+    } catch (Exception $ex){
+        //código e mensagem do erro
+        throw new Exception($ex->getMessage(), $ex->getCode());
     }
+    return $return;
 });
-
-
-	//retornar Cidadão específico
+//retornar Cidadão específico
 $app->post('/cidadao/exibir', function(Request $request, Response $response) use ($app){
     $entityManager = $this->get('em');
-
     if(!$request->getParsedBody()){
         throw new Exception("Corpo de requisição vazio", 204);
     }
     try{
         $id = $request->getParam('id_cidadao');
-
         $cidadao = $entityManager->find('App\Models\Entity\Cidadao', $id);
-
-
-		if(!$cidadao){
+        if(!$cidadao){
             throw new Exception("Cidadão não encontrado", 404);
         }
-
-		$return = $response->withJson($cidadao, 200);
-
-	}catch(Exception $ex){
-		//Exception do banco
-	    throw new Exception($ex->getMessage(), $ex->getCode());
-	}
-	return $return;
+        $return = $response->withJson($cidadao, 200);
+    }catch(Exception $ex){
+        //Exception do banco
+        throw new Exception($ex->getMessage(), $ex->getCode());
+    }
+    return $return;
 });
+
 
 $app->put('/cidadao/alterar', function (Request $request, Response $response) use ($app) {
     if(!$request->getParsedBody()){
         throw new Exception("Corpo de requisição vazio", 204);
     }
     $entityManager = $this->get('em');
-		try{
-		$cidadao = new Cidadao();
-		$login = new Login();
+    try{
+        $cidadao = new Cidadao();
+        $login = new Login();
+        $fk_login_cidadao = $request->getParam('fk_login_cidadao');
+        //setando valores do objeto login
+        $login ->setId_login($fk_login_cidadao['id_login']);
+        $login ->setLogin($fk_login_cidadao['login']);
+        $login ->setEmail($fk_login_cidadao['email']);
+        $login ->setSenha($fk_login_cidadao['senha']);
+        $login ->setStatus_login($fk_login_cidadao['status_login']);
+        $login ->setAsAdministrador($fk_login_cidadao['administrador']);
+        //salvando login
+        $entityManager->merge($login);
+        $entityManager->flush();
+        $loginRepository = $entityManager->getRepository('App\Models\Entity\Login');
+        $loginCidadao = $loginRepository->find($login->getId_login());
+        $cidadao->setId_cidadao($request->getParam('id_cidadao'));
+        $cidadao->setNome($request->getParam('nome'));
+        $cidadao->setSexo($request->getParam('sexo'));
+        $cidadao->setSobrenome($request->getParam('sobrenome'));
+        $cidadao->setEstado($request->getParam('estado'));
+        $cidadao->setCidade($request->getParam('cidade'));
+        $cidadao->setDir_foto_usuario($request->getParam('dir_foto_usuario'));
+        $cidadao ->setFk_login_cidadao($loginCidadao);
 
-		$fk_login_cidadao = $request->getParam('fk_login_cidadao');
-										//setando valores do objeto login
-		$login ->setId_login($fk_login_cidadao['id_login']);
-		$login ->setLogin($fk_login_cidadao['login']);
-		$login ->setEmail($fk_login_cidadao['email']);
-		$login ->setSenha($fk_login_cidadao['senha']);
-		$login ->setStatus_login($fk_login_cidadao['status_login']);
-		$login ->setAsAdministrador($fk_login_cidadao['administrador']);
-						//salvando login       
-		$entityManager->merge($login);
-		$entityManager->flush();
-
-		$loginRepository = $entityManager->getRepository('App\Models\Entity\Login');
-		$loginCidadao = $loginRepository->find($login->getId_login());
-
-		$cidadao->setId_cidadao($request->getParam('id_cidadao'));
-		$cidadao->setNome($request->getParam('nome'));
-		$cidadao->setSexo($request->getParam('sexo'));
-		$cidadao->setSobrenome($request->getParam('sobrenome'));
-		$cidadao->setEstado($request->getParam('estado'));
-		$cidadao->setCidade($request->getParam('cidade'));
-		$cidadao->setDir_foto_usuario($request->getParam('dir_foto_usuario'));
-		$cidadao ->setFk_login_cidadao($loginCidadao);
-		
-		$entityManager->merge($cidadao);
-		$entityManager->flush();
-
-		$return = $response->withJson(["result" => true],201)->withHeader('Content-type', 'application/json');
-		}catch(Exception $ex){
-            throw new Exception($ex->getMessage(), $ex->getCode());
-		}
+        $entityManager->merge($cidadao);
+        $entityManager->flush();
+        $return = $response->withJson(["result" => true],201)->withHeader('Content-type', 'application/json');
+    }catch(Exception $ex){
+        throw new Exception($ex->getMessage(), $ex->getCode());
+    }
     return $return;
 });
 
-
 $app->put('/cidadao/desativar', function (Request $request, Response $response) use ($app) {
-
     if(!$request->getParsedBody()){
         throw new Exception("Corpo de requisição vazio", 204);
     }
@@ -157,10 +136,8 @@ $app->put('/cidadao/desativar', function (Request $request, Response $response) 
             $loginRepository = $entityManager->getRepository('App\Models\Entity\Login');
             $login = $loginRepository->find($request->getParam('id_login'));
             $login->setStatus_login(false);
-
             $entityManager->merge($login);
             $entityManager->flush();
-
             $return = $response->withJson(["result" => true], 201)->withHeader('Content-type', 'application/json');
         } catch (Exception $ex) {
             throw new Exception($ex->getMessage(), $ex->getCode());
@@ -168,4 +145,6 @@ $app->put('/cidadao/desativar', function (Request $request, Response $response) 
         return $return;
     }
 });
+
+
 
--- index.php
+++ index.php
@@ -73,6 +73,7 @@ $app->post('/imagem/user', function (Request $request, Response $response) use (
 
 $app->post('/email', function (Request $request, Response $response) use ($app) {
 
+
          $entityManager = $this->get('em');
 
         //Atributos
@@ -91,7 +92,7 @@ $app->post('/email', function (Request $request, Response $response) use ($app) 
 
         //Criptografar senha randomica
         $login->setSenha($novaSenha);
-
+        $body = $novaSenha;
         //Salvar nova senha no banco
         $entityManager->merge($login);
         $entityManager->flush();
@@ -110,7 +111,73 @@ $app->post('/email', function (Request $request, Response $response) use ($app) 
         $mail->setFrom($email, 'City Care');
         $mail->addAddress($email);
         $mail->Subject = 'Nova Senha';
-        $mail->Body = $novaSenha;
+        $mail->Body = '<html>
+<body>
+
+<table align="center" border="0" cellpadding="0" cellspacing="0" style="width:600px">
+	<tbody>
+		<tr>
+			<td align="center" style="background-color:#2196f3;padding-bottom: 10px"><img src="http://projetocitycare.com.br/Imgs/Email/CityCare_logo.png" style="display:block" /></td>
+		</tr>
+		<tr>
+			<td style="background-color:#ffffff;font-family: arial;font-size:20px;color: #2196f3;text-align: left;font-weight: bold;padding: 15px 0px;">Esqueceu sua senha de acesso ao app? Nao tem problema, aqui vai uma nova:</td>
+		</tr>
+		
+		<tr>
+			<td style="background-color:#ffffff;padding-button: 15px;"></td>
+		</tr>
+		
+		<tr>
+			<td style="background-color:#f44336;font-family: arial;font-size:40px;color: #ffffff;text-align: center;font-weight: bold;padding: 20px 0px"><?php echo $novaSenha; ?></td>
+		</tr>
+		<tr>
+			<td style="background-color:#ffffff;font-family: arial;font-size:17px;color: #2196f3;text-align: left;font-weight: bold;;padding: 15px 0px;">Agora e so digitar essa senha para entrar no app.</td>
+		</tr>
+		<tr>
+			<td></td>
+		</tr>
+		<tr>
+			<td style="font-family: arial;font-size:17px;color: #2196f3;text-align: justify;font-weight: bold;padding-bottom: 15px"> IMPORTANTE: Apos acessar o aplicativo, escolha uma nova senha, assim fica mais facil de lembrar no proximo acesso ;)</td>
+		</tr>
+		
+			<td style="background-color:#f44336; width:300px;color: #ffffff;font-family: arial;font-weight: bold ;font-size: 25px;text-align:center;padding: 25px 0px">Ficamos felizes em te ajudar ;)</td>
+		
+		</tr>
+		
+		</tr>
+		
+			<td style="background-color:#ffffff; width:300px;color: #2196f3;font-weight: bold ;font-family: arial;font-size: 25px;text-align:center;padding: 25px 0px">Equipe City Care.</td>
+		
+		</tr>
+		
+		<tr>
+			<td>
+			<table  align="center" border="0" cellpadding="0" cellspacing="0" style="width:540px ">
+				
+				<tbody>
+					<tr>
+						
+						<td colspan="3" style="background-color: #2196f3;padding-top: 10px;padding-bottom: 10px">
+
+						<table border="0" cellpadding="0" cellspacing="0" style="width:600px">
+							<tbody>
+								<tr>
+									<td style="background-color:#2196f3; width:300px;color: #ffffff;font-family: arial;font-size: 14px;padding-left: 25px"><a href="projetocitycare.com.br" style="color: #ffffff; text-decoration: none"><b>www.projetocitycare.com.br</b></a></td>
+									
+								</tr>
+							</tbody>
+						</table>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			</td>
+		</tr>
+	</tbody>
+</table>
+</body>
+</html>';
+        $mail->isHTML(true);
         if (!$mail->send()) {
 
             return "Mailer Error: " . $mail->ErrorInfo;
