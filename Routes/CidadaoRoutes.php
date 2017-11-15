3e4b64cefae2870d940497718ed1c3ebfee5e3c5
João Victor Modesto <jvictorgmodesto@gmail.com>
2017-11-15 02:23:25 -0300

    Alterado o método alterar do cidadao e denuncia

--- Routes/CidadaoRoutes.php
+++ Routes/CidadaoRoutes.php
@@ -102,38 +102,71 @@ $app->post('/cidadao/exibir', function(Request $request, Response $response) use
 
 
 $app->put('/cidadao/alterar', function (Request $request, Response $response) use ($app) {
-    if(!$request->getParsedBody()){
-        throw new Exception("Corpo de requisição vazio", 204);
-    }
-    $entityManager = $this->get('em');
+    $entityManager = $this -> get('em');
+
+    $json = $request->getParam('cidadao');
+    $json = json_decode($json);
+
+
     try{
-        $cidadao = new Cidadao();
+        //Instância da entidade Login
         $login = new Login();
-        $fk_login_cidadao = $request->getParam('fk_login_cidadao');
+        //recuperando o parâmetro objeto login do json
+        $fk_login_cidadao = $json -> fk_login_cidadao;
         //setando valores do objeto login
-        $login ->setId_login($fk_login_cidadao['id_login']);
-        $login ->setLogin($fk_login_cidadao['login']);
-        $login ->setEmail($fk_login_cidadao['email']);
-        $login ->setSenha($fk_login_cidadao['senha']);
-        $login ->setStatus_login($fk_login_cidadao['status_login']);
-        $login ->setAsAdministrador($fk_login_cidadao['administrador']);
+        $login ->setLogin($fk_login_cidadao -> login);
+        $login ->setEmail($fk_login_cidadao -> email);
+        $login ->setSenha($fk_login_cidadao -> senha);
+        $login ->setStatus_login($fk_login_cidadao -> status_login);
+        $login ->setAsAdministrador($fk_login_cidadao -> administrador);
+
         //salvando login
         $entityManager->merge($login);
         $entityManager->flush();
+        //buscando login recém salvo
         $loginRepository = $entityManager->getRepository('App\Models\Entity\Login');
+        //pegando login
         $loginCidadao = $loginRepository->find($login->getId_login());
-        $cidadao->setId_cidadao($request->getParam('id_cidadao'));
-        $cidadao->setNome($request->getParam('nome'));
-        $cidadao->setSexo($request->getParam('sexo'));
-        $cidadao->setSobrenome($request->getParam('sobrenome'));
-        $cidadao->setEstado($request->getParam('estado'));
-        $cidadao->setCidade($request->getParam('cidade'));
-        $cidadao->setDir_foto_usuario($request->getParam('dir_foto_usuario'));
-        $cidadao ->setFk_login_cidadao($loginCidadao);
+        //Salvar foto
+        $files = $request->getUploadedFiles();
+        $newimage = $files['foto'];
+        if($newimage){
+            if ($newimage->getError() === UPLOAD_ERR_OK) {
+                $uploadFileName = $newimage->getClientFilename();
+                $type = $newimage->getClientMediaType();
+                $name = uniqid('img-' . date('d-m-y') . '-');
+                $name .= $newimage->getClientFilename();
+                //  $imgs[] = array('url' => '/Photos/' . $name);
+                //local server
+                $newimage->moveTo("/home/citycare/imgs/$name");#/home/citycare/Imgs/User/$name
+                //localdev
+                $photoURL = "http://projetocitycare.com.br/Imgs/User/$name";#/home/citycare/Imgs/User/$name
+            }}
+        else {
+            if ($json->sexo = "masculino") {
+                $photoURL = "http://projetocitycare.com.br/Imgs/User/Masculino.jpg";
+            }
+            else if ($json->sexo = "feminino") {
 
+                $photoURL = "http://projetocitycare.com.br/Imgs/User/Feminino.jpg";}
+        }
+
+
+
+        //Instância da entidade Cidadao
+        $cidadao = new Cidadao();
+        //setando valores do objeto cidadao
+        $cidadao ->setFk_login_cidadao($loginCidadao);
+        $cidadao->setNome($json -> nome);
+        $cidadao->setSexo($json -> sexo);
+        $cidadao ->setSobrenome($json -> sobrenome);
+        $cidadao ->setEstado($json -> estado);
+        $cidadao ->setCidade($json -> cidade);
+        $cidadao ->setDir_foto_usuario($photoURL);
         $entityManager->merge($cidadao);
         $entityManager->flush();
-        $return = $response->withJson(["result" => true],201)->withHeader('Content-type', 'application/json');
+        //retornando confirmação do evento completo
+        $return = $response->withStatus(201)->withHeader('Content-type', 'application/json');
     }catch(Exception $ex){
         throw new Exception($ex->getMessage(), $ex->getCode());
     }
--- Routes/DenunciaRoutes.php
+++ Routes/DenunciaRoutes.php
@@ -88,40 +88,70 @@ $app->post('/denuncia/cadastrar', function (Request $request, Response $response
 $app->put('/denuncia/alterar', function (Request $request, Response $response) use ($app) {
 
     $entityManager = $this->get('em');
-
     if(!$request->getParsedBody()){
         throw new Exception("Corpo de requisição vazio", 204);
     }else {
+        $entityManager = $this->get('em');
+
+        $json = $request->getParam('denuncia');
+        $json = json_decode($json);
+
+
         try{
+            $fk_login_denuncia = $json -> fk_login_denuncia;
+            $fk_categoria_denuncia = $json -> fk_categoria_denuncia;
+
             $denuncia = new Denuncia();
 
-            $fk_login_denuncia = ($request->getParam('fk_login_denuncia'));
-            $fk_categoria_denuncia= ($request->getParam('fk_categoria_denuncia'));
 
             $loginRepository = $entityManager->getRepository('App\Models\Entity\Login');
-            $loginDenuncia = $loginRepository->find($fk_login_denuncia['id_login']);
+            $loginDenuncia = $loginRepository->find($fk_login_denuncia -> id_login);
+
+
 
             $categoriaRepository = $entityManager->getRepository('App\Models\Entity\Categoria');
-            $categoriaDenuncia = $categoriaRepository->find($fk_categoria_denuncia['id_categoria']);
+            $categoriaDenuncia = $categoriaRepository->find($fk_categoria_denuncia -> id_categoria);
 
 
-            $denuncia->setId_denuncia($request->getParam('id_denuncia'));
-            $denuncia->setDescricao_denuncia($request->getParam('descricao_denuncia'));
-            $denuncia->setDir_foto_denuncia($request->getParam('dir_foto_denuncia'));
-            $denuncia->setLatitude_denuncia($request->getParam('latitude_denuncia'));
-            $denuncia->setLongitude_denuncia($request->getParam('longitude_denuncia'));
-            $denuncia->setData_denuncia($request->getParam('data_denuncia'));
-            $denuncia->setStatus_denuncia($request->getParam('status_denuncia'));
-            $denuncia->setCidade($request->getParam('cidade'));
-            $denuncia->setEstado($request->getParam('estado'));
+            //Salvar foto
+            $files = $request->getUploadedFiles();
+            $newimage = $files['fotoDenuncia'];
+            if($newimage){
+                if ($newimage->getError() === UPLOAD_ERR_OK) {
+                    $uploadFileName = $newimage->getClientFilename();
+                    $type = $newimage->getClientMediaType();
+                    $name = uniqid('img-' . date('d-m-y') . '-');
+                    $name .= $newimage->getClientFilename();
+                    //  $imgs[] = array('url' => '/Photos/' . $name);
+                    //local server
+                    $newimage->moveTo("/home/citycare/public_html/Imgs/Denuncia/$name");#/home/citycare/Imgs/User/$name
+                    //localdev
+                    $photoURL = "http://projetocitycare.com.br/Imgs/Denuncia/$name";#/home/citycare/Imgs/User/$name
+                }
+            }
+            else {
+                $photoURL = null;
+            }
+
+
+
+            //setando os campos da denuncia
+            $denuncia->setDescricao_denuncia($json -> descricao_denuncia);
+            $denuncia->setDir_foto_denuncia($photoURL);
+            $denuncia->setLatitude_denuncia($json -> latitude_denuncia);
+            $denuncia->setLongitude_denuncia($json -> longitude_denuncia);
+            $denuncia->setData_denuncia($json -> data_denuncia);
+            $denuncia->setStatus_denuncia($json -> status_denuncia);
             $denuncia->setFk_login_denuncia($loginDenuncia);
             $denuncia->setFk_categoria_denuncia($categoriaDenuncia);
+            $denuncia->setCidade($json -> cidade);
+            $denuncia->setEstado($json -> estado);
+
+
             $entityManager->merge($denuncia);
             $entityManager->flush();
 
-
-            $return = $response->withJson(["result" => true],201)->withHeader('Content-type', 'application/json');
-
+            $return = $response->withJson($denuncia,201)->withHeader('Content-type', 'application/json');
         }catch(Exception $ex)
         {
             throw new Exception($ex->getMessage(), $ex->getCode());
@@ -246,7 +276,7 @@ $app->post('/denuncia/feedSecundario', function (Request $request, Response $res
 
             foreach ($comentario as $comentarios){
                 $arrayComentario[] = ["fk_login_comentario" => array("id_login" => $comentarios -> fk_login_comentario -> id_login),
-                    "descricao" => $comentarios -> descricao_comentario,
+                    "descricao_comentario" => $comentarios -> descricao_comentario,
                     "fk_denuncia_comentario" => array( "id_denuncia" => $comentarios -> fk_denuncia_comentario -> id_denuncia),
                 ];
             }
@@ -287,17 +317,17 @@ $app->post('/denuncia/feedSecundario', function (Request $request, Response $res
         throw new Exception($ex->getMessage(), $ex->getCode());
     }
 
+    $arrayReverso = array_reverse($array);
 
-    return $response->withJson($array,200)->withHeader('Content-type', 'application/json');
-
-
+    return $response->withJson($arrayReverso,200)->withHeader('Content-type', 'application/json');
 });
+
 $app->get('/denuncia/all', function (Request $request, Response $response) use ($app) {
 
     $entityManager = $this->get('em');
 
     $denunciaRepository = $entityManager->getRepository('App\Models\Entity\Denuncia');
-    $denuncia = $denunciaRepository->findBy(array("status_denuncia" =>  array(0,1)));
+    $denuncia = $denunciaRepository->findBy(array("status_denuncia" => array(0,1)));
 
     $arrayDenuncia = array();
     $array = array();
@@ -307,66 +337,63 @@ $app->get('/denuncia/all', function (Request $request, Response $response) use (
 
     try{
 
-    foreach($denuncia as $index){
+        foreach($denuncia as $index){
 
-        $arrayAgiliza = array();
-        $agiliza = $agilizaRepository->findBy(array('fk_denuncia_agiliza' => $index -> id_denuncia, 'interacao' => 1));
 
-        foreach ($agiliza as $agilizas){
-            $arrayAgiliza[] = ["fk_login_agiliza" => array("id_login" => $agilizas -> fk_login_agiliza -> id_login),
-                "interacao" => $agilizas -> interacao,
-                "fk_denuncia_agiliza" => array( "id_denuncia" => $agilizas -> fk_denuncia_agiliza -> id_denuncia),
-                
+            $arrayAgiliza = array();
+            $agiliza = $agilizaRepository->findBy(array('fk_denuncia_agiliza' => $index -> id_denuncia, 'interacao' => 1));
+
+            foreach ($agiliza as $agilizas){
+                $arrayAgiliza[] = ["fk_login_agiliza" => array("id_login" => $agilizas -> fk_login_agiliza -> id_login),
+                    "interacao" => $agilizas -> interacao,
+                    "fk_denuncia_agiliza" => array( "id_denuncia" => $agilizas -> fk_denuncia_agiliza -> id_denuncia),
+
+                ];
+            }
+
+            $arrayComentario = array();
+            $comentario = $comentarioRepository->findBy(array('fk_denuncia_comentario' => $index -> id_denuncia));
+
+            foreach ($comentario as $comentarios){
+                $arrayComentario[] = ["fk_login_comentario" => array("id_login" => $comentarios -> fk_login_comentario -> id_login),
+                    "descricao_comentario" => $comentarios -> descricao_comentario,
+                    "fk_denuncia_comentario" => array( "id_denuncia" => $comentarios -> fk_denuncia_comentario -> id_denuncia),
+                ];
+            }
+
+            $arrayDenuncia[] = ["id_denuncia" => $index ->id_denuncia,
+                "fk_login_denuncia" => array( "id_login" => $index -> fk_login_denuncia -> id_login),
+                "descricao_denuncia" => $index -> descricao_denuncia,
+                "dir_foto_denuncia" => $index -> dir_foto_denuncia,
+                "latitude_denuncia" => $index -> latitude_denuncia,
+                "longitude_denuncia" => $index -> longitude_denuncia,
+                "cidade" => $index -> cidade,
+                "estado" => $index -> estado,
+                "data_denuncia" => $index -> data_denuncia,
+                "status_denuncia" => $index -> status_denuncia,
+                "fk_categoria_denuncia" => $index -> fk_categoria_denuncia,
+                "fk_solucao_denuncia" => $index -> fk_solucao_denuncia,
+
             ];
+
+            $array[] = ["denuncia" => $arrayDenuncia[$i],"agiliza" => $arrayAgiliza, "comentario" => $arrayComentario];
+
+
+            $i++;
+
         }
-
-        $arrayComentario = array();
-        $comentario = $comentarioRepository->findBy(array('fk_denuncia_comentario' => $index -> id_denuncia));
-
-        foreach ($comentario as $comentarios){
-            $arrayComentario[] = ["fk_login_comentario" => array("id_login" => $comentarios -> fk_login_comentario -> id_login),
-                "descricao" => $comentarios -> descricao_comentario,
-                "fk_denuncia_comentario" => array( "id_denuncia" => $comentarios -> fk_denuncia_comentario -> id_denuncia),
-            ];
-        }
-
-        $arrayDenuncia[] = ["id_denuncia" => $index ->id_denuncia,
-            "fk_login_denuncia" => array( "id_login" => $index -> fk_login_denuncia -> id_login),
-            "descricao_denuncia" => $index -> descricao_denuncia,
-            "dir_foto_denuncia" => $index -> dir_foto_denuncia,
-            "latitude_denuncia" => $index -> latitude_denuncia,
-            "longitude_denuncia" => $index -> longitude_denuncia,
-            "cidade" => $index -> cidade,
-            "estado" => $index -> estado,
-            "data_denuncia" => $index -> data_denuncia,
-            "status_denuncia" => $index -> status_denuncia,
-            "fk_categoria_denuncia" => $index -> fk_categoria_denuncia,
-            "fk_solucao_denuncia" => $index -> fk_solucao_denuncia,
-
-        ];
-
-
-
-
-
-
-
-
-
-
-
-
-        $array[] = ["denuncia" => $arrayDenuncia[$i],"agiliza" => $arrayAgiliza, "comentario" => $arrayComentario];
-
-
-        $i++;
-
-    }
     }catch(Exception $ex){
         throw new Exception($ex->getMessage(), $ex->getCode());
     }
 
 
-  return $response->withJson($array,200)->withHeader('Content-type', 'application/json');
-});
+    $arrayReverso = array_reverse($array);
+    $novoArray = array();
 
+    for ($o = 0;$o <= 5; $o++) {
+        $novoArray[$o] = $arrayReverso[$o];
+    }
+
+
+    return $response->withJson($novoArray,200)->withHeader('Content-type', 'application/json');
+});
